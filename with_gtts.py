c91c332dd53afa0c71ba928d7168457cb8287c2e
Rishav Mitra <rishavmitra10@gmail.com>
2021-10-17 13:16:05 +0000

    Version that uses gTTS (google text-to-speech)

---
+++ with_gtts.py
@@ -0,0 +1,30 @@
+from gtts import gTTS
+import PyPDF2
+import PySimpleGUI as sg
+
+def pdf_to_sound_file(filename):
+	input_file_name = filename
+	output_file_name = input_file_name.replace('.pdf', '.mp3')
+
+	# The Backend
+	''' Read the pdf file
+		Initialise the text-to-speech function
+		Extract the text from each page
+		Save the final text as an mp3 file
+	'''
+	with open(input_file_name, 'rb') as file:
+		reader = PyPDF2.PdfFileReader(file)
+
+		final_text = ''
+		for page in range(reader.numPages):
+			page = reader.getPage(page)
+			text = page.extractText()
+
+			final_text += text
+
+		audio = gTTS(final_text)
+		audio.save(output_file_name)
+
+filename = sg.popup_get_file('Enter the file you wish to process')
+if filename:
+	pdf_to_sound_file(filename)
