454b1c6f2c1e9be5d2298ceb80d68acef80d8fe0
Rishav-12 <rishavmitra10@gmail.com>
2021-11-14 16:42:48 +0000

    comment formatting

--- with_gtts.py
+++ with_gtts.py
@@ -7,10 +7,11 @@ def pdf_to_sound_file(filename):
 	output_file_name = input_file_name.replace('.pdf', '.mp3')
 
 	# The Backend
-	''' Read the pdf file
-		Initialise the text-to-speech function
-		Extract the text from each page
-		Save the final text as an mp3 file
+	'''
+	Read the pdf file
+	Initialise the text-to-speech function
+	Extract the text from each page
+	Save the final text as an mp3 file
 	'''
 	with open(input_file_name, 'rb') as file:
 		reader = PyPDF2.PdfFileReader(file)
